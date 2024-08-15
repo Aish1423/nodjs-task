51d893b4841809507e4e286b6d94d82915ee068e
Aishwarya <132289284+Aish1423@users.noreply.github.com>
2024-08-16 00:02:57 +0530

    Add files via upload

---
+++ res.js
@@ -0,0 +1,292 @@
+var search_result= [
+    {
+      "type": "restaurant",
+      "info": {
+        "resId": 19382015,
+        "name": "Cake Wala",
+        "image": {
+          "url": ""
+        },
+        "o2FeaturedImage": {
+          "url": "https://b.zmtcdn.com/data/pictures/5/19382015/e5c8141547a6f5268b5f157484acf6d9_o2_featured_v2.jpg"
+        },
+        "rating": {
+          "has_fake_reviews": 0,
+          "aggregate_rating": "3.7",
+          "rating_text": "3.7",
+          "rating_subtitle": "Good",
+          "rating_color": "9ACD32",
+          "votes": "4,573",
+          "subtext": "REVIEWS",
+          "is_new": false
+        },
+        "ratingNew": {
+          "newlyOpenedObj": null,
+          "suspiciousReviewObj": null,
+          "ratings": {
+            "DINING": {
+              "rating_type": "DINING",
+              "rating": "3.5",
+              "reviewCount": "48",
+              "reviewTextSmall": "48 Reviews",
+              "subtext": "48 Dining Reviews",
+              "color": "#1C1C1C",
+              "ratingV2": "3.5",
+              "subtitle": "DINING",
+              "sideSubTitle": "Dining Reviews",
+              "bgColorV2": {
+                "type": "green",
+                "tint": "500"
+              }
+            },
+            "DELIVERY": {
+              "rating_type": "DELIVERY",
+              "rating": "3.9",
+              "reviewCount": "4,525",
+              "reviewTextSmall": "4,525 Reviews",
+              "subtext": "4,525 Delivery Reviews",
+              "color": "#E23744",
+              "ratingV2": "3.9",
+              "subtitle": "DELIVERY",
+              "sideSubTitle": "Delivery Reviews",
+              "bgColorV2": {
+                "type": "green",
+                "tint": "600"
+              },
+              "newOnDelivery": false
+            }
+          }
+        },
+        "cft": {
+          "text": "₹300 for two"
+        },
+        "cfo": {
+          "text": "₹350 for one"
+        },
+        "locality": {
+          "name": "Agra Cantt, Agra",
+          "address": "11- A, Ward 8, Kishan Garh, Idgah, Agra Cantt, Agra",
+          "localityUrl": "agra/agra-cantt-restaurants"
+        },
+        "timing": {
+          "text": "",
+          "color": ""
+        },
+        "cuisine": [
+          {
+            "deeplink": "zomato://search?deeplink_filters=WyJ7XCJjb250ZXh0XCI6XCJhbGxcIn0iLCJ7XCJjdWlzaW5lX2lkXCI6W1wiNVwiXX0iXQ%3D%3D",
+            "url": "https://www.zomato.com/agra/restaurants/bakery/",
+            "name": "Bakery"
+          }
+        ],
+        "should_ban_ugc": false,
+        "costText": {
+          "text": "₹350 for one"
+        }
+      },
+      "order": {
+        "deliveryTime": "31 min",
+        "isServiceable": true,
+        "hasOnlineOrdering": true,
+        "actionInfo": {
+          "text": "Order Now",
+          "clickUrl": "/agra/cake-wala-agra-cantt/order"
+        }
+      },
+      "gold": [],
+      "takeaway": [],
+      "cardAction": {
+        "text": "",
+        "clickUrl": "/agra/cake-wala-agra-cantt/order?contextual_menu_params=eyJkaXNoX3NlYXJjaCI6eyJ0aXRsZSI6IkJlc3QgaW4gQ2FrZSIsImRpc2hfaWRzIjpbIjM5MjEyIl0sImN1aXNpbmVfaWRzIjpbXX19",
+        "clickActionDeeplink": ""
+      },
+      "distance": "2.7 km",
+      "isPromoted": true,
+      "promotedText": "Promoted",
+      "trackingData": [
+        {
+          "table_name": "zsearch_events_log",
+          "payload": "{\"search_id\":\"44f6dadf-e372-4551-96db-370212bdca93\",\"location_type\":\"delivery_cell\",\"location_id\":\"4140058016516931584\",\"page_type\":\"delivery\",\"app_type\":\"new_web_consumer\",\"section\":\"restaurants\",\"entity_type\":\"restaurant\",\"entity_id\":\"19382015\",\"element_type\":\"listing\",\"rank\":1,\"metadata\":{\"is_ad\":\"true\"}}",
+          "event_names": {
+            "tap": "{\"action\":\"tap\"}",
+            "impression": "{\"action\":\"impression\"}"
+          }
+        }
+      ],
+      "allCTA": [],
+      "promoOffer": "",
+      "checkBulkOffers": true,
+      "bulkOffers": [
+        {
+          "text": "50% OFF up to 100",
+          "color": {
+            "tint": "500",
+            "type": "blue"
+          }
+        }
+      ],
+      "isDisabled": false,
+      "bottomContainers": [
+        {
+          "image": {
+            "url": "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png"
+          },
+          "text": "2525+ orders placed from here recently"
+        },
+        {
+          "image": {
+            "url": "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+            "aspect_ratio": 2.66666666667
+          },
+          "text": "Follows all Max Safety measures to ensure your food is safe"
+        }
+      ]
+    },
+    {
+      "type": "restaurant",
+      "info": {
+        "resId": 20052578,
+        "name": "Cake Company",
+        "image": {
+          "url": "https://b.zmtcdn.com/data/dish_photos/af0/a8b1fcaca223d15b528a02c212022af0.jpg"
+        },
+        "o2FeaturedImage": {
+          "url": "https://b.zmtcdn.com/data/pictures/8/20052578/f26a2c2079232d330b37b0305b689795_o2_featured_v2.jpg"
+        },
+        "rating": {
+          "has_fake_reviews": 0,
+          "aggregate_rating": "3.6",
+          "rating_text": "3.6",
+          "rating_subtitle": "Good",
+          "rating_color": "9ACD32",
+          "votes": "352",
+          "subtext": "REVIEWS",
+          "is_new": false
+        },
+        "ratingNew": {
+          "newlyOpenedObj": null,
+          "suspiciousReviewObj": null,
+          "ratings": {
+            "DINING": {
+              "rating_type": "DINING",
+              "rating": "",
+              "reviewCount": "0",
+              "reviewTextSmall": "0 Reviews",
+              "subtext": "Does not offer Dining",
+              "color": "",
+              "ratingV2": "-",
+              "subtitle": "DINING",
+              "sideSubTitle": "Dining Reviews",
+              "bgColorV2": {
+                "type": "grey",
+                "tint": "500"
+              }
+            },
+            "DELIVERY": {
+              "rating_type": "DELIVERY",
+              "rating": "3.6",
+              "reviewCount": "352",
+              "reviewTextSmall": "352 Reviews",
+              "subtext": "352 Delivery Reviews",
+              "color": "#E23744",
+              "ratingV2": "3.6",
+              "subtitle": "DELIVERY",
+              "sideSubTitle": "Delivery Reviews",
+              "bgColorV2": {
+                "type": "green",
+                "tint": "600"
+              },
+              "newOnDelivery": false
+            }
+          }
+        },
+        "cft": {
+          "text": "₹350 for two"
+        },
+        "cfo": {
+          "text": "₹350 for one"
+        },
+        "locality": {
+          "name": "Agra Cantt, Agra",
+          "address": "11-A, Kishan Garh, Idgah Colony, Agra Cantt, Agra",
+          "localityUrl": "agra/agra-cantt-restaurants"
+        },
+        "timing": {
+          "text": "",
+          "color": ""
+        },
+        "cuisine": [
+          {
+            "deeplink": "zomato://search?deeplink_filters=WyJ7XCJjb250ZXh0XCI6XCJhbGxcIn0iLCJ7XCJjdWlzaW5lX2lkXCI6W1wiMTAwXCJdfSJd",
+            "url": "https://www.zomato.com/agra/restaurants/desserts/",
+            "name": "Desserts"
+          },
+          {
+            "deeplink": "zomato://search?deeplink_filters=WyJ7XCJjb250ZXh0XCI6XCJhbGxcIn0iLCJ7XCJjdWlzaW5lX2lkXCI6W1wiNVwiXX0iXQ%3D%3D",
+            "url": "https://www.zomato.com/agra/restaurants/bakery/",
+            "name": "Bakery"
+          }
+        ],
+        "should_ban_ugc": false,
+        "costText": {
+          "text": "₹350 for one"
+        }
+      },
+      "order": {
+        "deliveryTime": "34 min",
+        "isServiceable": true,
+        "hasOnlineOrdering": true,
+        "actionInfo": {
+          "text": "Order Now",
+          "clickUrl": "/agra/cake-company-agra-cantt/order"
+        }
+      },
+      "gold": [],
+      "takeaway": [],
+      "cardAction": {
+        "text": "",
+        "clickUrl": "/agra/cake-company-agra-cantt/order?contextual_menu_params=eyJkaXNoX3NlYXJjaCI6eyJ0aXRsZSI6IkJlc3QgaW4gQ2FrZSIsImRpc2hfaWRzIjpbIjM5MjEyIl0sImN1aXNpbmVfaWRzIjpbXX19",
+        "clickActionDeeplink": ""
+      },
+      "distance": "2.8 km",
+      "isPromoted": false,
+      "promotedText": "",
+      "trackingData": [
+        {
+          "table_name": "zsearch_events_log",
+          "payload": "{\"search_id\":\"44f6dadf-e372-4551-96db-370212bdca93\",\"location_type\":\"delivery_cell\",\"location_id\":\"4140058016516931584\",\"page_type\":\"delivery\",\"app_type\":\"new_web_consumer\",\"section\":\"restaurants\",\"entity_type\":\"restaurant\",\"entity_id\":\"20052578\",\"element_type\":\"listing\",\"rank\":2}",
+          "event_names": {
+            "tap": "{\"action\":\"tap\"}",
+            "impression": "{\"action\":\"impression\"}"
+          }
+        }
+      ],
+      "allCTA": [],
+      "promoOffer": "",
+      "checkBulkOffers": true,
+      "bulkOffers": [
+        {
+          "text": "50% OFF up to 100",
+          "color": {
+            "tint": "500",
+            "type": "blue"
+          }
+        }
+      ],
+      "isDisabled": false,
+      "bottomContainers": [
+        {
+          "image": {
+            "url": "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png"
+          },
+          "text": "925+ orders placed from here recently"
+        },
+        {
+          "image": {
+            "url": "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png",
+            "aspect_ratio": 2.66666666667
+          },
+          "text": "Follows all Max Safety measures to ensure your food is safe"
+        }
+      ]
+    },
